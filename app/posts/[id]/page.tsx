e3b2a050129007ea9f0a472b20d2ffbef8250f0b
Grzegorz Martowski <grzegorz.martowski@impbrands.com>
2026-02-03 16:28:21 +0100

    Codebase init

---
+++ app/posts/[id]/page.tsx
@@ -0,0 +1,26 @@
+import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
+import {notFound} from "next/navigation";
+
+// @ts-ignore
+export default async function Page({params}) {
+    const id = (await params).id
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const posts = await response.json();
+
+    if(!posts) {
+        notFound()
+    }
+
+    return (
+        <div>
+            <h1>Posts Page</h1>
+            {posts.length > 0 && posts.map((post: { id: Key | null | undefined; title: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; body: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; }) => (
+            <div key={post.id}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </div>
+        ))}
+      <p>This is the posts page.</p>
+    </div>
+  );
+}
